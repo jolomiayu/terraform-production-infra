eef97e5758728291a1ef6db6b7db0b90b4d869cb
Ubuntu <ubuntu@ip-172-31-38-4.eu-west-1.compute.internal>
2026-05-28 14:33:03 +0000

    Add PostgresSQL healthchecks orchestration

--- modules/autoscaling/main.tf
+++ modules/autoscaling/main.tf
@@ -1,4 +1,5 @@
 resource "aws_launch_template" "web_lt" {
+
   name_prefix   = "terraform-web-lt-"
   image_id      = var.ami_id
   instance_type = var.instance_type
@@ -11,15 +12,31 @@ resource "aws_launch_template" "web_lt" {
 
   user_data = base64encode(<<-EOF
 #!/bin/bash
+
 apt update -y
-apt install nginx -y
-echo "<h1>Terraform Auto Scaling Server</h1>" > /var/www/html/index.html
-systemctl enable nginx
-systemctl start nginx
+
+apt install -y docker.io git curl
+
+systemctl start docker
+systemctl enable docker
+
+curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+chmod +x /usr/local/bin/docker-compose
+
+cd /home/ubuntu
+
+git clone https://github.com/jolomiayu/terraform-production-infra.git
+
+cd terraform-production-infra/app
+
+docker-compose up -d
+
 EOF
   )
 
   tag_specifications {
+
     resource_type = "instance"
 
     tags = {
@@ -29,6 +46,7 @@ EOF
 }
 
 resource "aws_autoscaling_group" "web_asg" {
+
   name             = "terraform-web-asg"
   desired_capacity = 2
   min_size         = 2
@@ -44,6 +62,7 @@ resource "aws_autoscaling_group" "web_asg" {
   ]
 
   launch_template {
+
     id      = aws_launch_template.web_lt.id
     version = "$Latest"
   }
@@ -51,6 +70,7 @@ resource "aws_autoscaling_group" "web_asg" {
   health_check_type = "ELB"
 
   tag {
+
     key                 = "Name"
     value               = "terraform-asg-web"
     propagate_at_launch = true
